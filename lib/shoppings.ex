7d455febc8778343c509dcedb92000e1150c278a
gissandrogama <gissandrogama@gmail.com>
2021-01-20 21:44:29 -0300

    start project

---
+++ lib/shoppings.ex
@@ -0,0 +1,18 @@
+defmodule Shoppings do
+  @moduledoc """
+  Documentation for `Shoppings`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Shoppings.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/shoppings_test.exs
@@ -0,0 +1,8 @@
+defmodule ShoppingsTest do
+  use ExUnit.Case
+  doctest Shoppings
+
+  test "greets the world" do
+    assert Shoppings.hello() == :world
+  end
+end
